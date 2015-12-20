2e4ef4b92667388af60f21e43095d835769840b5
David Nemeth <dt-dave@hotmail.com>
2015-12-20 14:21:30 +0100

    NewsSite Initial

---
+++ MvcNews/MvcNews/Models/NewsEntities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcNews.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Tittle { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public bool Published { get; set; }
+        [Required]
+        public DateTime PostedDate { get; set; }
+        public DateTime? Modified { get; set; }
+        public Category Category { get; set; }
+        public ICollection<PostTag> PostTags { get; set; }
+    }
+    public class PostTag
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<News> News { get; set; }
+    }
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<News> News { get; set; }
+    }
+
+}
---
+++ MvcNews/MvcNews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcNews.Startup))]
+namespace MvcNews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
