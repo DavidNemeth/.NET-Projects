8216f2b22013ba663ad0b2d63b20f9723312fe3d
David Nemeth <dt-dave@hotmail.com>
2016-01-13 03:25:27 +0100

    CartViewModel

---
+++ KisKer/KisKer/ViewModels/CartViewModel.cs
@@ -0,0 +1,67 @@
+﻿using KisKer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KisKer.ViewModels
+{
+    public class CartViewModel
+    {
+        public ErtekesitesReszletekModel DataModell { get; set; }
+        public string ReturnUrl { get; set; }
+        public class ErtekesitesReszletekLine
+        {
+            public int AruID { get; set; }
+            public decimal EgysegAr { get; set; }
+            public int AruMennyiseg { get; set; }
+            public string AruNeve { get; set; }
+        }
+        public class ErtekesitesReszletekModel
+        {
+            private List<ErtekesitesReszletekLine> BevasarloLista = new List<ErtekesitesReszletekLine>();
+            public IEnumerable<ErtekesitesReszletekLine> RendelesiTetelek
+            {
+                get
+                {
+                    return (BevasarloLista);
+                }
+            }
+            public void AddTetel(AruKeszlet Aru, int nMennyiseg)
+            {
+                ErtekesitesReszletekLine tetel = BevasarloLista.
+                  Where(p => p.AruID == Aru.AruID).
+                  FirstOrDefault();
+                if (tetel == null)
+                {
+                    BevasarloLista.Add(new ErtekesitesReszletekLine
+                    {
+                        AruID = Aru.AruID,
+                        EgysegAr = Aru.EgysegAr,
+                        AruMennyiseg = 1,
+                        AruNeve = Aru.AruMegnevezes                        
+                    });
+                }
+                else
+                {
+                    tetel.AruMennyiseg += (short)nMennyiseg;
+                }
+            }
+
+            public void RemoveTetel(AruKeszlet Aru)
+            {
+                BevasarloLista.RemoveAll(p => p.AruID == Aru.AruID);
+            }
+
+            public decimal TeljesErtek()
+            {
+                return (BevasarloLista.Sum(p => Convert.ToDecimal(p.AruMennyiseg) * p.EgysegAr));
+            }
+
+            public void ClearAll()
+            {
+                BevasarloLista.Clear();
+            }
+        }
+    }
+}
