42b3e407ea35044277bd96822b67c0256a5e3fac
David Nemeth <dt-dave@hotmail.com>
2016-01-18 07:08:25 +0100

    Validation

--- KisKer/KisKer/App_Start/RouteConfig.cs
+++ KisKer/KisKer/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KisKer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "NapiAdatok", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
