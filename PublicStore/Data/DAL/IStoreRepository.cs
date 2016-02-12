f5b036a86561477d37084be0427eb54fef3ea042
David Nemeth <dt-dave@hotmail.com>
2016-02-12 06:24:34 +0100

    Initial Commit

---
+++ PublicStore/Data/DAL/IStoreRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DAL
+{
+    public interface IStoreRepository : IDisposable
+    {
+        IList<AruKategoria> GetCategories();
+        IList<AruKeszlet> GetAruKeszletek();
+        IList<Ertekesites> GetErtekesitesek();
+        IList<ErtekesitesReszlet> GetErtekesitesReszletek();
+        IList<AruKeszlet> KeszletForCategory(int CategoryID);
+        IList<ErtekesitesReszlet> GetAruErtekesitesReszletek(AruKeszlet reszletek);
+        IList<AruKeszlet> GetArukForCategory(AruKategoria aruk);
+        IList<ErtekesitesReszlet> GetErtekesitesreszletForErtekesites(Ertekesites ertekesites);
+
+    }
+}
