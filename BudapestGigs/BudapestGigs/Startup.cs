d0460ac248474b5bea1339db5e447f81d2ee5721
Dávid Németh <dt-dave@hotmail.com>
2017-05-17 03:19:28 +0200

    Initial commit with migrations

---
+++ BudapestGigs/BudapestGigs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BudapestGigs.Startup))]
+namespace BudapestGigs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
