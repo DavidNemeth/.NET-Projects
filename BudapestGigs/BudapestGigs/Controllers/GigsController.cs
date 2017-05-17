0deed1d054b7281069feb625d90be0b316aeb447
Dávid Németh <dt-dave@hotmail.com>
2017-05-17 09:07:38 +0200

    no message

---
+++ BudapestGigs/BudapestGigs/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using BudapestGigs.Models;
+using BudapestGigs.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BudapestGigs.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var gigviewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(gigviewModel);
+        }
+    }
+}
