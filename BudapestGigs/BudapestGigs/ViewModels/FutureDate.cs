17cfbc941e1cb67d7d3c93b26fe285a414e14f31
Dávid Németh <dt-dave@hotmail.com>
2017-05-18 02:26:06 +0200

    Implemented Validation.

---
+++ BudapestGigs/BudapestGigs/ViewModels/FutureDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace BudapestGigs.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            bool isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "dd MM yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out dateTime);
+
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
