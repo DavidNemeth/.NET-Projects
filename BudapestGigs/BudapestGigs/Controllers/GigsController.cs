e7addef0d32a1d8372af5e19152a98ff3a90fb5b
Dávid Németh <dt-dave@hotmail.com>
2017-05-18 04:56:19 +0200

    Implemented AntiForgeryToken.

--- BudapestGigs/BudapestGigs/Controllers/GigsController.cs
+++ BudapestGigs/BudapestGigs/Controllers/GigsController.cs
@@ -27,6 +27,7 @@ namespace BudapestGigs.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
